5d41e2fd3e7f24f38707f42ffdda13fe6c4b06c4
Fortune <fortuneochi@gmail.com>
2020-11-10 12:03:05 +0100

    added comment in app.tsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+    // I just modify stuff
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
